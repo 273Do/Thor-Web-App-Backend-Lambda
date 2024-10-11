fbf62f27a11de465a3c267b683a7bf822e6d9647
273* <keita.y310@icloud.com>
2024-10-11 20:18:30 +0900

    🔥 S3の署名付きurlを発行するようにしました．

--- thor-web-app-be/app.py
+++ thor-web-app-be/app.py
@@ -1,46 +1,36 @@
 from flask import Flask, jsonify, request
-import boto3
 import uuid
-import os
-from dotenv import load_dotenv
-
-# envファイルの読み込み
-load_dotenv()
-
-# s3のバケット名
-bucket_name = os.environ['AWS_S3_BUCKET_NAME']
-
-# s3のクライアントを作成
-s3 = boto3.client('s3')
+import json
+from src.s3_functions import uploadS3
 
 # FlaskのWebアプリ作成
 app = Flask(__name__)
 
 
-# POSTリクエストとエンドポイントの設定
-@app.route("/", methods=['POST'])
+# GETリクエストとエンドポイントの設定
+@app.route("/get_presigned_url", methods=['GET'])
 def main():
-    # リクエストにファイルが含まれているか確認
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part in the request"}), 400
 
-    # ファイルの取得
-    file = request.files['file']
+    # クエリパラメータからファイル名を取得
+    file_name = request.args.get('file_name')
+    if not file_name:
+        return jsonify({"error": "file_name parameter is required"}), 400
 
-    # ファイル名が空か確認
-    if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
+    # 一時ファイル名を生成
+    UUID = str(uuid.uuid4())  # uuidを生成
+    tmp_file = f"{UUID}/{file_name}"
 
-    # ファイルのアップロード処理
-    try:
-        # s3へのディレクトリの作成
-        tmp_dir = str(uuid.uuid4())
-        save_path = tmp_dir + "/" + file.filename
-
-        # s3へのアップロード処理
-        s3.upload_fileobj(file, bucket_name, save_path)
-        return jsonify(message='File uploaded successfully : ' + save_path), 200
-
-    except Exception as e:
-        # エラーが発生した場合はエラーメッセージを返す
-        return jsonify({"error": str(e)}), 500
+    # s3のプリサインドurlの発行
+    success, error_message, presigned_url = uploadS3(tmp_file)
+    if success:
+        print(presigned_url)
+        return jsonify({
+            'message': 'successfully',
+            'body': json.dumps(
+                {
+                    'PUT_URL': presigned_url,
+                    'UUID': UUID
+                }
+            )}), 200
+    else:
+        return {"status": "failed", "error_message": error_message}, 500
---
+++ thor-web-app-be/src/s3_functions.py
@@ -0,0 +1,39 @@
+import os
+import boto3
+from botocore.client import Config
+from dotenv import load_dotenv
+
+# envファイルの読み込み
+load_dotenv()
+
+# s3のバケット名
+bucket_name = os.environ['AWS_S3_BUCKET_NAME']
+
+# プリサインドURLの有効期間
+duration_seconds = int(os.environ['DURATION_SECONDS'])
+
+# リージョンの設定
+region = os.environ['AWS_DEFAULT_REGION']
+
+# s3のクライアントを作成
+s3 = boto3.client('s3', region_name=region,
+                  config=Config(signature_version='s3v4'))
+
+# s3のプリサインドurlの発行処理
+
+
+def uploadS3(save_path):
+    try:
+        presigned_url = s3.generate_presigned_url(
+            ClientMethod='put_object',
+            Params={
+                'Bucket': bucket_name,
+                'Key': save_path,
+                'ContentType': 'application/zip'
+            },
+            ExpiresIn=duration_seconds,
+            HttpMethod='PUT'
+        )
+        return True, None, presigned_url
+    except Exception as e:
+        return False, str(e), None
