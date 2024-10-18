7c6fe70d2c0c7f1a8daabbbfe1ce2b933b823fd2
273* <keita.y310@icloud.com>
2024-10-18 19:12:42 +0900

    ✨ テスト用のデータを読み込めるようにしました．

--- thor-web-app-be/src/analysis/main.py
+++ thor-web-app-be/src/analysis/main.py
@@ -1,9 +1,19 @@
+import pandas as pd
+
 # データ解析用のメイン関数
 
 
 def data_analyze(step_count_df, sleep_analysis_df):
 
-    print(step_count_df)
-    print(sleep_analysis_df)
+    print(step_count_df.head())
+    print(sleep_analysis_df.head())
 
     return True, None, None
+
+
+# テスト用のデータを読み込む
+# CSVファイルを読み込む
+sc_df = pd.read_csv('./test/StepCountCP.csv')
+sa_df = pd.read_csv('./test/SleepAnalysisCP.csv')
+
+data_analyze(sc_df, sa_df)
