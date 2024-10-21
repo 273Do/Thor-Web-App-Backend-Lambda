ae6b9f23aadc7296e6033b3253b0be673cd7b3c4
273* <keita.y310@icloud.com>
2024-10-21 12:10:02 +0900

    ➕ Relativedeltaを追加しました．

--- thor-web-app-be/src/analysis/auxiliary_functions.py
+++ thor-web-app-be/src/analysis/auxiliary_functions.py
@@ -6,9 +6,9 @@ import pandas as pd
 # データを最後のレコードから直近の月数に絞る関数
 def narrow_the_data(df, months):
 
-    # 開始月と終了月を初期化
-    start_month = 0
-    end_month = 0
+    # 開始と終了を初期化
+    start_date = 0
+    end_date = 0
 
     # 指定の日付範囲でフィルタリング
     # df = df[(df["startDate"] >= time["time"]["start_date"])
@@ -18,21 +18,28 @@ def narrow_the_data(df, months):
     df_last = df.iloc[-1]
 
     print(df_last['startDate'])
-    print(type(df_last['startDate']))
 
     # 最後のデータの月を取得
-    end_month = df_last['startDate'].month
+    end_date = df_last['startDate']
 
-    print(end_month)
+    print(end_date)
 
     # 精査するデータの範囲を月単位で設定
-    if (end_month - months) < 0:
-        start_month = end_month - months + 13
-    else:
-        start_month = end_month - months + 1
+    # if (end_date.month - months) < 0:
+    #     start_date = end_date - months + 13
+    # else:
+    #     start_date = end_date - months + 1
 
-    print(start_month, end_month)
+    if (end_date.month - months) < 0:
+        start_date = 0
+
+    print(start_date, end_date)
 
     # 設定した月でデータを絞る
+
+    narrow_df = df[(df["startDate"] >= start_date)]
+    #    & (df["startDate"] <= end_date)]
+    print(narrow_df.head())
+    print(narrow_df.tail())
 
     pass
--- thor-web-app-be/src/analysis/main.py
+++ thor-web-app-be/src/analysis/main.py
@@ -24,13 +24,13 @@ def data_analyze(step_count_df, sleep_analysis_df):
 # CSVファイルを読み込む
 
 # MEMO: コンテナ内で実行する場合は以下の2列のパスを使用
-# sc_df = pd.read_csv('./test/StepCountCP.csv', low_memory=False)
-# sa_df = pd.read_csv(
-#     './test/SleepAnalysisCP.csv', low_memory=False)
-sc_df = pd.read_csv(
-    './thor-web-app-be/src/analysis/test/SleepAnalysisCP.csv', low_memory=False)
+sc_df = pd.read_csv('./test/StepCountCP.csv', low_memory=False)
 sa_df = pd.read_csv(
-    './thor-web-app-be/src/analysis/test/SleepAnalysisCP.csv', low_memory=False)
+    './test/SleepAnalysisCP.csv', low_memory=False)
+# sc_df = pd.read_csv(
+#     './thor-web-app-be/src/analysis/test/SleepAnalysisCP.csv', low_memory=False)
+# sa_df = pd.read_csv(
+#     './thor-web-app-be/src/analysis/test/SleepAnalysisCP.csv', low_memory=False)
 
 # 必要な列だけを抽出する
 sc_df = sc_df[["sourceVersion", "startDate", "endDate", "value"]]
